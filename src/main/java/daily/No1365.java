40114c3f5efb73ae786d66254ba181dd764e5b29
a <a@a.a>
2020-10-26 10:53:39 +0800

    add 1365 2020-10-26

---
+++ src/main/java/daily/No1365.java
@@ -0,0 +1,78 @@
+package daily;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class No1365 {
+    public int[] smallerNumbersThanCurrent1(int[] nums) {
+        int n = nums.length;
+        int[] ret = new int[n];
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                if (nums[j] < nums[i]) {
+                    cnt++;
+                }
+            }
+            ret[i] = cnt;
+        }
+        return ret;
+    }
+
+    public int[] smallerNumbersThanCurrent2(int[] nums) {
+        int n = nums.length;
+        int[][] data = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            data[i][0] = nums[i];
+            data[i][1] = i;
+        }
+        Arrays.sort(data, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] data1, int[] data2) {
+                return data1[0] - data2[0];
+            }
+        });
+
+        int[] ret = new int[n];
+        int prev = -1;
+        for (int i = 0; i < n; i++) {
+            if (prev == -1 || data[i][0] != data[i - 1][0]) {
+                prev = i;
+            }
+            ret[data[i][1]] = prev;
+        }
+        return ret;
+    }
+
+    public int[] smallerNumbersThanCurrent3(int[] nums) {
+        int[] cnt = new int[101];
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            cnt[nums[i]]++;
+        }
+        for (int i = 1; i <= 100; i++) {
+            cnt[i] += cnt[i - 1];
+        }
+        int[] ret = new int[n];
+        for (int i = 0; i < n; i++) {
+            ret[i] = nums[i] == 0 ? 0 : cnt[nums[i] - 1];
+        }
+        return ret;
+    }
+
+    public int[] smallerNumbersThanCurrent4(int[] nums) {
+        int[] sort = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(sort);
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < sort.length; j++) {
+                if (nums[i] > sort[j]) {
+                    result[i]++;
+                } else {
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
