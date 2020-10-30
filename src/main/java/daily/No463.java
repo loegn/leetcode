6fb6400e16ceb5b3d82e975cd751e18775fd8ba1
a <a@a.a>
2020-10-30 10:35:04 +0800

    add 463 2020-10-30

---
+++ src/main/java/daily/No463.java
@@ -0,0 +1,60 @@
+package daily;
+
+public class No463 {
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
+    public int islandPerimeter(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == 1) {
+                    ans += dfs(i, j, grid, n, m);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public int dfs(int x, int y, int[][] grid, int n, int m) {
+        if (x < 0 || x >= n || y < 0 || y >= m || grid[x][y] == 0) {
+            return 1;
+        }
+        if (grid[x][y] == 2) {
+            return 0;
+        }
+        grid[x][y] = 2;
+        int res = 0;
+        for (int i = 0; i < 4; ++i) {
+            int tx = x + dx[i];
+            int ty = y + dy[i];
+            res += dfs(tx, ty, grid, n, m);
+        }
+        return res;
+    }
+
+    public int islandPerimeter2(int[][] grid) {
+        int result = 0;
+        int[][] forward = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    for (int m = 0; m < forward.length; m++) {
+                        int i1 = i + forward[m][0];
+                        int j1 = j + forward[m][1];
+                        if (i1 < 0 || i1 >= grid.length || j1 < 0 || j1 >= grid[i1].length || grid[i1][j1] == 0) {
+                            result++;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        No463 no463 = new No463();
+        no463.islandPerimeter(new int[][]{{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}});
+    }
+}
