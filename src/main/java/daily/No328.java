a7ceaf5a1fd943b23e9eacd2a4224bf102079abd
a <a@a.a>
2020-11-13 10:41:48 +0800

    add 328 2020-11-13 328. 奇偶链表

---
+++ src/main/java/daily/No328.java
@@ -0,0 +1,42 @@
+package daily;
+
+import pojo.ListNode;
+
+/**
+ * 给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+ * 请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
+ */
+public class No328 {
+    public ListNode oddEvenList1(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode evenHead = head.next;
+        ListNode odd = head, even = evenHead;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+
+    public ListNode oddEvenList2(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        ListNode odd = head;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
