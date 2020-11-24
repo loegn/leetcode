f85c47082074f094a5574bd22c1ac937bdaff262
a <a@a.a>
2020-11-24 10:04:20 +0800

    add 222 2020-11-24 完全二叉树的节点个数

---
+++ src/main/java/daily/No222.java
@@ -0,0 +1,54 @@
+package daily;
+
+import pojo.TreeNode;
+
+public class No222 {
+    public int countNodes1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int level = 0;
+        TreeNode node = root;
+        while (node.left != null) {
+            level++;
+            node = node.left;
+        }
+        int low = 1 << level, high = (1 << (level + 1)) - 1;
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if (exists(root, level, mid)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public boolean exists(TreeNode root, int level, int k) {
+        int bits = 1 << (level - 1);
+        TreeNode node = root;
+        while (node != null && bits > 0) {
+            if ((bits & k) == 0) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+            bits >>= 1;
+        }
+        return node != null;
+    }
+
+    public int countNodes2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        int count = 1;
+        count += countNodes2(root.left);
+        count += countNodes2(root.right);
+        return count;
+    }
+}
