9a233fb0e98945acf9147787acdf8cbd54b5a8a7
a <a@a.a>
2020-11-25 09:46:17 +0800

    add 1370 2020-11-25 上升下降字符串

---
+++ src/main/java/daily/No1370.java
@@ -0,0 +1,42 @@
+package daily;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 给你一个字符串 s ，请你根据下面的算法重新构造字符串：
+ * 从 s 中选出 最小 的字符，将它 接在 结果字符串的后面。
+ * 从 s 剩余字符中选出 最小 的字符，且该字符比上一个添加的字符大，将它 接在 结果字符串后面。
+ * 重复步骤 2 ，直到你没法从 s 中选择字符。
+ * 从 s 中选出 最大 的字符，将它 接在 结果字符串的后面。
+ * 从 s 剩余字符中选出 最大 的字符，且该字符比上一个添加的字符小，将它 接在 结果字符串后面。
+ * 重复步骤 5 ，直到你没法从 s 中选择字符。
+ * 重复步骤 1 到 6 ，直到 s 中所有字符都已经被选过。
+ * 在任何一步中，如果最小或者最大字符不止一个 ，你可以选择其中任意一个，并将其添加到结果字符串。
+ */
+public class No1370 {
+    public String sortString(String s) {
+        int[] num = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            num[s.charAt(i) - 'a']++;
+        }
+
+        StringBuffer ret = new StringBuffer();
+        while (ret.length() < s.length()) {
+            for (int i = 0; i < 26; i++) {
+                if (num[i] > 0) {
+                    ret.append((char) (i + 'a'));
+                    num[i]--;
+                }
+            }
+            for (int i = 25; i >= 0; i--) {
+                if (num[i] > 0) {
+                    ret.append((char) (i + 'a'));
+                    num[i]--;
+                }
+            }
+        }
+        return ret.toString();
+    }
+}
