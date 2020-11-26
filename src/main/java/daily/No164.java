ce45b31f40e03a8632f1587b05ed0c7d674de02e
a <a@a.a>
2020-11-26 11:27:08 +0800

    add 164 2020-11-26 最大间距

---
+++ src/main/java/daily/No164.java
@@ -0,0 +1,108 @@
+package daily;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode-cn.com/problems/maximum-gap/solution/zui-da-jian-ju-by-leetcode-solution/
+ * 给定一个无序的数组，找出数组在排序之后，相邻元素之间最大的差值。
+ * 如果数组元素个数小于 2，则返回 0。
+ * 你可以假设数组中所有元素都是非负整数，且数值在 32 位有符号整数范围内。
+ * 请尝试在线性时间复杂度和空间复杂度的条件下解决此问题。
+ */
+public class No164 {
+    /**
+     * 基于桶的算法
+     */
+    public int maximumGap(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return 0;
+        }
+        int minVal = Arrays.stream(nums).min().getAsInt();
+        int maxVal = Arrays.stream(nums).max().getAsInt();
+        int d = Math.max(1, (maxVal - minVal) / (n - 1));
+        int bucketSize = (maxVal - minVal) / d + 1;
+
+        int[][] bucket = new int[bucketSize][2];
+        for (int i = 0; i < bucketSize; ++i) {
+            Arrays.fill(bucket[i], -1); // 存储 (桶内最小值，桶内最大值) 对， (-1, -1) 表示该桶是空的
+        }
+        for (int i = 0; i < n; i++) {
+            int idx = (nums[i] - minVal) / d;
+            if (bucket[idx][0] == -1) {
+                bucket[idx][0] = bucket[idx][1] = nums[i];
+            } else {
+                bucket[idx][0] = Math.min(bucket[idx][0], nums[i]);
+                bucket[idx][1] = Math.max(bucket[idx][1], nums[i]);
+            }
+        }
+
+        int ret = 0;
+        int prev = -1;
+        for (int i = 0; i < bucketSize; i++) {
+            if (bucket[i][0] == -1) {
+                continue;
+            }
+            if (prev != -1) {
+                ret = Math.max(ret, bucket[i][0] - bucket[prev][1]);
+            }
+            prev = i;
+        }
+        return ret;
+    }
+
+    /**
+     * 基数排序
+     */
+    public int maximumGap2(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return 0;
+        }
+        long exp = 1;
+        int[] buf = new int[n];
+        int maxVal = Arrays.stream(nums).max().getAsInt();
+
+        while (maxVal >= exp) {
+            int[] cnt = new int[10];
+            for (int i = 0; i < n; i++) {
+                int digit = (nums[i] / (int) exp) % 10;
+                cnt[digit]++;
+            }
+            for (int i = 1; i < 10; i++) {
+                cnt[i] += cnt[i - 1];
+            }
+            for (int i = n - 1; i >= 0; i--) {
+                int digit = (nums[i] / (int) exp) % 10;
+                buf[cnt[digit] - 1] = nums[i];
+                cnt[digit]--;
+            }
+            System.arraycopy(buf, 0, nums, 0, n);
+            exp *= 10;
+        }
+
+        int ret = 0;
+        for (int i = 1; i < n; i++) {
+            ret = Math.max(ret, nums[i] - nums[i - 1]);
+        }
+        return ret;
+    }
+
+    /**
+     * 非线性复杂度
+     */
+    public int maximumGap3(int[] nums) {
+        Arrays.sort(nums);
+        int max = 0;
+        for (int i = 1; i < nums.length; i++) {
+            max = Math.max(max, nums[i] - nums[i - 1]);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        No164 no164 = new No164();
+        int[] input = {73, 22, 93, 43, 55, 14, 28, 65, 39, 81};
+        no164.maximumGap2(input);
+    }
+}
