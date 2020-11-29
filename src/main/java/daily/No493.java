f851bdf3640b2299edeb6e2327d15c281bedf96a
anonymous <anonymous@anonymous.com>
2020-11-29 20:48:59 +0800

    add daily 2020-11-28

---
+++ src/main/java/daily/No493.java
@@ -0,0 +1,67 @@
+package daily;
+
+/**
+ * https://leetcode-cn.com/problems/reverse-pairs/
+ * 给定一个数组 nums ，如果 i < j 且 nums[i] > 2*nums[j] 我们就将 (i, j) 称作一个重要翻转对。
+ * 你需要返回给定数组中的重要翻转对的数量。
+ */
+public class No493 {
+    public int reversePairs(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        return reversePairsRecursive(nums, 0, nums.length - 1);
+    }
+
+    /**
+     * 归并排序
+     * @param nums
+     * @param left
+     * @param right
+     * @return
+     */
+    public int reversePairsRecursive(int[] nums, int left, int right) {
+        if (left == right) {
+            return 0;
+        } else {
+            //反转对数目等于两个子数组反转对数目之和加上左数组所有节点在右数组的反转对数目之和（两个子数组都是已经排序的）
+            int mid = (left + right) / 2;
+            int n1 = reversePairsRecursive(nums, left, mid);
+            int n2 = reversePairsRecursive(nums, mid + 1, right);
+            int ret = n1 + n2;
+
+            // 首先统计左数组在右数组中的反转数组对的数量
+            int i = left;
+            int j = mid + 1;
+            while (i <= mid) {
+                while (j <= right && (long) nums[i] > 2 * (long) nums[j]) {
+                    j++;
+                }
+                ret += j - mid - 1;
+                i++;
+            }
+
+            // 随后合并两个排序数组并排序
+            int[] sorted = new int[right - left + 1];
+            int p1 = left, p2 = mid + 1;
+            int p = 0;
+            while (p1 <= mid || p2 <= right) {
+                if (p1 > mid) {
+                    sorted[p++] = nums[p2++];
+                } else if (p2 > right) {
+                    sorted[p++] = nums[p1++];
+                } else {
+                    if (nums[p1] < nums[p2]) {
+                        sorted[p++] = nums[p1++];
+                    } else {
+                        sorted[p++] = nums[p2++];
+                    }
+                }
+            }
+            for (int k = 0; k < sorted.length; k++) {
+                nums[left + k] = sorted[k];
+            }
+            return ret;
+        }
+    }
+}
