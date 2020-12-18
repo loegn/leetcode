4f6fbcf0e5bcb502586715dc683d36aa20586ccb
a <a@a.a>
2020-12-18 09:46:34 +0800

    add 714 2020-12-17 买卖股票的最佳时机含手续费

---
+++ src/main/java/daily/No714.java
@@ -0,0 +1,62 @@
+package daily;
+
+import utils.ParamUtils;
+import utils.PrintUtils;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 给定一个整数数组 prices，其中第 i 个元素代表了第 i 天的股票价格 ；非负整数 fee 代表了交易股票的手续费用。
+ * 你可以无限次地完成交易，但是你每笔交易都需要付手续费。如果你已经购买了一个股票，在卖出它之前你就不能再继续购买股票了。
+ * 返回获得利润的最大值。
+ * 注意：这里的一笔交易指买入持有并卖出股票的整个过程，每笔交易你只需要为支付一次手续费。
+ */
+public class No714 {
+    public int maxProfit1(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+
+    public int maxProfit2(int[] prices, int fee) {
+        int n = prices.length;
+        int sell = 0, buy = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            sell = Math.max(sell, buy + prices[i] - fee);
+            buy = Math.max(buy, sell - prices[i]);
+        }
+        return sell;
+    }
+
+    public int maxProfit3(int[] prices, int fee) {
+        int n = prices.length;
+        int buy = prices[0] + fee;
+        int profit = 0;
+        for (int i = 1; i < n; ++i) {
+            if (prices[i] + fee < buy) {
+                buy = prices[i] + fee;
+            } else if (prices[i] > buy) {
+                profit += prices[i] - buy;
+                buy = prices[i];
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        No714 no714 = new No714();
+        int[] prieces = ParamUtils.getInt("[1, 3, 2, 8, 4, 9]");
+        int fee = 2;
+        Object result = no714.maxProfit3(prieces, fee);
+        PrintUtils.print(result);
+    }
+}
