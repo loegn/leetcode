4b7b60165c267b9580f18e48a3fbdbd02c468b49
a <a@a.a>
2020-12-21 09:29:54 +0800

    modify no714

--- src/main/java/daily/No714.java
+++ src/main/java/daily/No714.java
@@ -15,10 +15,15 @@ import java.util.List;
  * 注意：这里的一笔交易指买入持有并卖出股票的整个过程，每笔交易你只需要为支付一次手续费。
  */
 public class No714 {
+    /**
+     * 动态规划
+     */
     public int maxProfit1(int[] prices, int fee) {
         int n = prices.length;
         int[][] dp = new int[n][2];
+        //第一天结束手里没股票
         dp[0][0] = 0;
+        //第一天结束手里有股票
         dp[0][1] = -prices[0];
         for (int i = 1; i < n; ++i) {
             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
@@ -27,6 +32,9 @@ public class No714 {
         return dp[n - 1][0];
     }
 
+    /**
+     * 动态规划-空间优化
+     */
     public int maxProfit2(int[] prices, int fee) {
         int n = prices.length;
         int sell = 0, buy = -prices[0];
