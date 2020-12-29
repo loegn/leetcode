9cd691fa6633f35dc20c50bc561eb7965ca6897a
a <a@a.a>
2020-12-29 09:31:26 +0800

    add 188 2020-12-29 卖股票的最佳时机 IV

---
+++ src/main/java/daily/No188.java
@@ -0,0 +1,84 @@
+package daily;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个整数数组 prices ，它的第 i 个元素 prices[i] 是一支给定的股票在第 i 天的价格。
+ * 设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ */
+public class No188 {
+    /**
+     * 动态规划
+     *
+     * @param k
+     * @param prices
+     * @return
+     */
+    public int maxProfit1(int k, int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+
+        int n = prices.length;
+        k = Math.min(k, n / 2);
+        int[][] buy = new int[n][k + 1];
+        int[][] sell = new int[n][k + 1];
+
+        buy[0][0] = -prices[0];
+        sell[0][0] = 0;
+        for (int i = 1; i <= k; ++i) {
+            buy[0][i] = sell[0][i] = Integer.MIN_VALUE / 2;
+        }
+
+        for (int i = 1; i < n; ++i) {
+            buy[i][0] = Math.max(buy[i - 1][0], sell[i - 1][0] - prices[i]);
+            for (int j = 1; j <= k; ++j) {
+                buy[i][j] = Math.max(buy[i - 1][j], sell[i - 1][j] - prices[i]);
+                sell[i][j] = Math.max(sell[i - 1][j], buy[i - 1][j - 1] + prices[i]);
+            }
+        }
+
+        return Arrays.stream(sell[n - 1]).max().getAsInt();
+    }
+
+    /**
+     * 动态规划-优化数组
+     * @param k
+     * @param prices
+     * @return
+     */
+    public int maxProfit2(int k, int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+
+        int n = prices.length;
+        k = Math.min(k, n / 2);
+        int[] buy = new int[k + 1];
+        int[] sell = new int[k + 1];
+
+        buy[0] = -prices[0];
+        sell[0] = 0;
+        for (int i = 1; i <= k; ++i) {
+            buy[i] = sell[i] = Integer.MIN_VALUE / 2;
+        }
+
+        for (int i = 1; i < n; ++i) {
+            buy[0] = Math.max(buy[0], sell[0] - prices[i]);
+            for (int j = 1; j <= k; ++j) {
+                buy[j] = Math.max(buy[j], sell[j] - prices[i]);
+                sell[j] = Math.max(sell[j], buy[j - 1] + prices[i]);
+            }
+        }
+
+        return Arrays.stream(sell).max().getAsInt();
+    }
+
+    public static void main(String[] args) {
+        No188 no188 = new No188();
+        int[] prices = {3, 2, 6, 5, 0, 3};
+        Object result = no188.maxProfit2(2, prices);
+        System.out.println(result);
+    }
+}
