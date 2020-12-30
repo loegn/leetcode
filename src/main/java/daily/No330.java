090a3c94c2f3124ba8dd6792191f1a06d93a205b
a <a@a.a>
2020-12-30 11:14:55 +0800

    add 330 2020-12-30 按要求补齐数组

---
+++ src/main/java/daily/No330.java
@@ -0,0 +1,30 @@
+package daily;
+
+/**
+ * 给定一个已排序的正整数数组 nums，和一个正整数 n 。从 [1, n] 区间内选取任意个数字补充到 nums 中，使得 [1, n] 区间内的任何数字都可以用 nums 中某几个数字的和来表示。请输出满足上述要求的最少需要补充的数字个数。
+ */
+public class No330 {
+    /**
+     * 贪心算法
+     * @param nums
+     * @param n
+     * @return
+     */
+    public int minPatches(int[] nums, int n) {
+        int patches = 0;
+        //未被数组nums覆盖的最小的整数
+        long x = 1;
+        int length = nums.length, index = 0;
+        while (x <= n) {
+            if (index < length && nums[index] <= x) {
+                x += nums[index];
+                index++;
+            } else {
+                x *= 2;
+                patches++;
+            }
+        }
+        return patches;
+    }
+
+}
